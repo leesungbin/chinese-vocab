281015092e9ee16e5fbffe4bd8b905387bf9aa0d
leesungbin <lee@sungbin.dev>
2025-07-21 21:53:50 +0900

    Fix github pages build issue

--- packages/frontend/next.config.mjs
+++ packages/frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.GITHUB_ACTIONS ? '/chinese-vocab' : '',
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
